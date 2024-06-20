4d6eebe1d2c5611132c75457638ee2c2eb19a599
AndreaPejrani <andreapejra@gmail.com>
2024-06-20 17:33:21 +0200

    Update sketch.js

--- Pioggia/sketch.js
+++ Pioggia/sketch.js
@@ -1,51 +1,52 @@
 let drops = [];
+let numDrops = 800; 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  for (let i = 0; i < 500; i++) {
+  for (let i = 0; i < numDrops; i++) {
     drops.push(new Drop());
   }
 }
 
 function draw() {
   background(0);
-
-  for (let drop of drops) {
+  drops.forEach(drop => {
     drop.fall();
     drop.display();
-  }
+  });
 }
 
 class Drop {
   constructor() {
+    this.reset();
+  }
+
+  reset() {
     this.x = random(width);
     this.y = random(-500, -50);
     this.z = random(0, 20);
-    this.len = map(this.z, 0, 20, 10, 20);
-    this.yspeed = map(this.z, 0, 20, 4, 10);
-    this.xspeed = map(this.z, 0, 20, 1, 3);
+    this.len = map(this.z, 0, 20, 10, 25);
+    this.yspeed = map(this.z, 0, 20, 5, 15);
+    this.xspeed = random(-1, 1); 
+    this.thickness = map(this.z, 0, 20, 1, 2); 
+    this.color = color(173, 216, 230, 150); 
   }
 
   fall() {
     this.y += this.yspeed;
     this.x += this.xspeed;
-    let grav = map(this.z, 0, 20, 0, 0.2);
-    this.yspeed += grav;
-
+    this.yspeed += map(this.z, 0, 20, 0, 0.2); 
     if (this.y > height) {
-      this.y = random(-200, -100);
-      this.yspeed = map(this.z, 0, 20, 4, 10);
-      this.x = random(width);
+      this.reset();
     }
-    if (this.x > width || this.x < 0) {
+    if (this.x > width + 20 || this.x < -20) {
       this.x = random(width);
     }
   }
 
   display() {
-    let thickness = map(this.z, 0, 20, 1, 3);
-    stroke(173, 216, 230); // Colore azzurro per la pioggia
-    strokeWeight(thickness);
-    line(this.x, this.y, this.x, this.y + this.len);
+    strokeWeight(this.thickness);
+    stroke(this.color);
+    line(this.x, this.y, this.x + this.xspeed * 5, this.y + this.len); 
   }
 }
